c8c185714f3a3547cba2096305153977ecdf8d1e
Jared Alwyn <jaredalwyn22@gmail.com>
2018-08-07 13:39:30 -0500

    Updated student course search. Can now search student Id and get feedback on what name is associated.

--- CourseProject/CourseProject/StudentViewCourseForm.Designer.cs
+++ CourseProject/CourseProject/StudentViewCourseForm.Designer.cs
@@ -29,12 +29,12 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.studentIdTextBox = new System.Windows.Forms.TextBox();
-            this.lblStudentName = new System.Windows.Forms.TextBox();
             this.btnFind = new System.Windows.Forms.Button();
+            this.lblStudentName = new System.Windows.Forms.TextBox();
+            this.studentIdTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.studentDataGridView = new System.Windows.Forms.DataGridView();
             this.btnClose = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
@@ -55,15 +55,31 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Student Data";
             // 
-            // label1
+            // btnFind
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(40, 50);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 24);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Student ID:";
+            this.btnFind.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFind.Location = new System.Drawing.Point(356, 51);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(103, 23);
+            this.btnFind.TabIndex = 5;
+            this.btnFind.Text = "&Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // lblStudentName
+            // 
+            this.lblStudentName.Enabled = false;
+            this.lblStudentName.Location = new System.Drawing.Point(193, 124);
+            this.lblStudentName.Name = "lblStudentName";
+            this.lblStudentName.Size = new System.Drawing.Size(266, 20);
+            this.lblStudentName.TabIndex = 4;
+            // 
+            // studentIdTextBox
+            // 
+            this.studentIdTextBox.Location = new System.Drawing.Point(193, 54);
+            this.studentIdTextBox.Name = "studentIdTextBox";
+            this.studentIdTextBox.Size = new System.Drawing.Size(141, 20);
+            this.studentIdTextBox.TabIndex = 3;
             // 
             // label2
             // 
@@ -75,6 +91,16 @@
             this.label2.TabIndex = 1;
             this.label2.Text = "Student Name:";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student ID:";
+            // 
             // label3
             // 
             this.label3.AutoSize = true;
@@ -84,32 +110,6 @@
             this.label3.Size = new System.Drawing.Size(246, 24);
             this.label3.TabIndex = 2;
             this.label3.Text = "All Courses by This Student:";
-            // 
-            // studentIdTextBox
-            // 
-            this.studentIdTextBox.Location = new System.Drawing.Point(163, 54);
-            this.studentIdTextBox.Name = "studentIdTextBox";
-            this.studentIdTextBox.Size = new System.Drawing.Size(141, 20);
-            this.studentIdTextBox.TabIndex = 3;
-            // 
-            // lblStudentName
-            // 
-            this.lblStudentName.Enabled = false;
-            this.lblStudentName.Location = new System.Drawing.Point(197, 124);
-            this.lblStudentName.Name = "lblStudentName";
-            this.lblStudentName.Size = new System.Drawing.Size(262, 20);
-            this.lblStudentName.TabIndex = 4;
-            // 
-            // btnFind
-            // 
-            this.btnFind.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnFind.Location = new System.Drawing.Point(340, 50);
-            this.btnFind.Name = "btnFind";
-            this.btnFind.Size = new System.Drawing.Size(103, 23);
-            this.btnFind.TabIndex = 5;
-            this.btnFind.Text = "&Find";
-            this.btnFind.UseVisualStyleBackColor = true;
-            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
             // 
             // studentDataGridView
             // 
--- CourseProject/CourseProject/StudentViewCourseForm.cs
+++ CourseProject/CourseProject/StudentViewCourseForm.cs
@@ -14,9 +14,18 @@ namespace CourseProject
 {
     public partial class StudentViewCourseForm : Form
     {
+        string connectionString;    // Declared string variable at the class level.
+        SqlConnection conn;         // SQLconnection variable.
+        int studentId;
+
         public StudentViewCourseForm()
         {
             InitializeComponent();
+            // Assign value to the string vaiable.
+            connectionString =
+                ConfigurationManager.ConnectionStrings
+                ["CourseProject.Properties.Settings.TinyCollegeDBConnectionString"]
+                .ConnectionString;
         }
 
         // Load event for the form. 
@@ -28,7 +37,30 @@ namespace CourseProject
         // Find button searches for student ID.
         private void btnFind_Click(object sender, EventArgs e)
         {
+            using (conn = new SqlConnection(connectionString))
+            using (SqlCommand comd = new SqlCommand
 
+             ("SELECT studentId, studentName, studentGrade FROM student" +
+            " WHERE  student.studentId = @studentId", conn))
+            using (SqlDataAdapter adapter = new SqlDataAdapter(comd))
+            {
+                comd.Parameters.AddWithValue("@studentId", studentIdTextBox.Text);
+                DataTable courseTable = new DataTable();
+                adapter.Fill(courseTable);
+
+                if (courseTable.Rows.Count < 1)
+                {
+                    lblStudentName.Clear();
+                    lblStudentName.Text = "No Student Found";
+                }
+                else
+                {
+                    DataRow dr = courseTable.Rows[0];
+                    studentId = int.Parse(dr["studentId"].ToString());
+                    lblStudentName.Text = dr["studentName"].ToString();
+                }
+
+            }
         }
 
         // Button will close the current form. 
